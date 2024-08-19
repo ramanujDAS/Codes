3e5f76335f97cdbbcbbb31c7b8c17c31bbf6fca7
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2024-08-19 17:50:03 +0530

    Time: 1 ms (91.27%) | Memory: 44.8 MB (97.10%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length ;
+        int m = isConnected[0].length ;
+       int [] visited = new int[n] ;
+       int ans = 0;
+     for(int i=0 ;i < n ;i++){
+        if(visited[i] == 0){
+                ans++;
+                fun(isConnected , i ,0,visited);
+            
+        }
+     }
+   return ans;
+    }
+
+    void fun(int[][] graph , int i, int j , int [] visited){
+    // if(visited[i]==1) return;
+         visited[i]=1;
+        for(int k =0 ;k< graph.length ; k++){
+                 if(graph[i][k] == 1 && visited[k] == 0)
+                      fun(graph,k,0,visited);
+        }
+       
+
+    }
+}
