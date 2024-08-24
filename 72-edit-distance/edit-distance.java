ad2776958c0dae4820d0c49287deb81e99fd76a6
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2024-08-25 00:57:48 +0530

    Time: 6 ms (15.30%) | Memory: 44.7 MB (87.47%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -3,9 +3,30 @@ class Solution {
 
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
         for (int row[] : dp)
-            Arrays.fill(row, -1);
+            Arrays.fill(row, 0);
 
-        return minDistance(word1, word2, 0, 0, dp);
+        for(int i = 0;i<=word1.length() ; i++){
+            for(int j = 0; j<=word2.length() ;j++){
+                
+                if(i==0){
+                    dp[i][j] = j;continue;
+                }
+                if(j==0){
+                    dp[i][j] = i;continue;
+                }
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] ;
+                }
+                else{
+                  dp[i][j] = 1 + Math.min(dp[i-1][j-1] , Math.min(dp[i-1][j], dp[i][j-1])) ;
+                }
+
+            }
+        }
+
+       return dp[word1.length()][word2.length()];
+
+        //return minDistance(word1, word2, 0, 0, dp);
     }
 
     private int minDistance(String word1, String word2, int i, int j, int[][] dp) {
@@ -19,7 +40,7 @@ class Solution {
         if (dp[i][j] != -1)
             return dp[i][j];
 
-        int minDis = 01;
+        int minDis = 0;
         if (word1.charAt(i) == word2.charAt(j)) {
             minDis = minDistance(word1, word2, i + 1, j + 1,dp);
         } else {
