6d2378d30e410d65ecde75a383a0b6b3bb569c22
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2024-12-02 19:02:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (7.88%) - LeetSync

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -3,12 +3,10 @@ class Solution {
 
         String[] words = sentence.split(" ");
         
-        for( int i =0 ;i < words.length ;i++){
-            if(words[i].length() >= searchWord.length()){
-                if(words[i].substring(0 , searchWord.length()).equals(searchWord))
-                {
-                    return i + 1;
-                } 
+        for( int i = 0 ;i < words.length ;i++){
+           int index =  words[i].indexOf(searchWord);
+            if(index == 0){
+              return i +1;
             }
         }
        return -1; 
