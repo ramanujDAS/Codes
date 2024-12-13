0a08380292f2c048c5c9b3d6976138ed7ab07636
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2024-12-13 22:39:34 +0530

    Time: 33 ms (11.82%) | Memory: 45.7 MB (7.44%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maximumLength(String s) {
+
+        int maxLength = -1;
+        for (int len = s.length() - 1; len > 0; len--) {
+            if (findLength(s, len))
+                maxLength = Math.max(maxLength, len);
+        }
+        return maxLength;
+    }
+
+    private boolean findLength(String s, int windowSize) {
+        Map<String, Integer> countMap = new HashMap<>();
+
+        int j = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while (j < s.length()) {
+
+            if (sb.length() > 0 && sb.charAt(sb.length() - 1) == s.charAt(j)) {
+                sb.append(s.charAt(j));
+            } else {
+                sb = new StringBuilder();
+                sb.append(s.charAt(j));
+            }
+
+            if (sb.length() > windowSize) {
+                sb.deleteCharAt(0);
+            }
+
+            if (sb.length() == windowSize) {
+                countMap.put(sb.toString(), countMap.getOrDefault(sb.toString(), 0) + 1);
+            }
+
+            if (countMap.getOrDefault(sb.toString(), 0) == 3)
+                return true;
+
+            j++;
+        }
+        return false;
+    }
+}
