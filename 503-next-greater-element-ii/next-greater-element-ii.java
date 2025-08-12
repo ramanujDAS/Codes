2b3b4342093108c34c472269c3e9545fca7aaae9
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2025-08-12 22:19:00 +0530

    Time: 18 ms (27.13%) | Memory: 45.8 MB (97.39%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,28 +1,28 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        Stack<Integer> st = new Stack<>();
-        int[] ans = new int[2 * n];
-        for (int i = 2 * n - 1; i >= 0; i--) {
 
-            while (!st.isEmpty() && st.peek() <= nums[i % n]) {
-                st.pop();
-            }
-            if (st.isEmpty()) {
-                ans[i] = -1;
-            } else {
-                ans[i] = st.peek();
-            }
-            st.push(nums[i % n]);
+        int [] ans = new int[2*n];
+         Stack<Integer> stack = new Stack<>();
+        for(int i = 2*n-1;i >=0 ;i-- ){
+           
+           while(!stack.isEmpty() && (stack.peek() <= nums[i%n]))
+              stack.pop();
+            
+           if(stack.isEmpty())
+              ans[i] = -1;
+            else{
+              ans[i] = stack.peek();
+            }  
+           
+           stack.push(nums[i%n]);
 
         }
+      int[] ansT = new int[n];
+       
+       for(int i = 0 ;i<n;i++)
+          ansT[i] = ans[i];
 
-        int[] ansT = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            ansT[i] = ans[i];
-        }
-
-        return ansT;
+       return ansT;   
     }
 }
