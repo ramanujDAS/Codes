a6f1e284e9f73600c1ba92e4fc3928e171702a66
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2025-09-06 22:39:39 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (77.62%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if( root == null)
+           return 0;
+
+        int leftHeight = getHeight(root , true);
+        int rightHeight = getHeight(root , false);
+
+        if(leftHeight == rightHeight){
+            return (1<<leftHeight) - 1;
+        }    
+
+
+       return 1 + countNodes(root.left) + countNodes(root.right); 
+
+    }
+
+    private int getHeight(TreeNode node, boolean isLeft) {
+        int height = 0;
+
+        while (node != null) {
+            height++;
+            if (isLeft)
+                node = node.left;
+            else
+                node = node.right;
+        }
+        return height;
+    }
+}
