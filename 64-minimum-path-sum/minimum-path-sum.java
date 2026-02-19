277e7d34d998a6bc6efd974cc4717b1d8abc7477
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2026-02-19 22:20:12 +0530

    Time: 6 ms (10.50%) | Memory: 50.4 MB (55.64%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,44 +1,41 @@
-;
 class Solution {
     public int minPathSum(int[][] grid) {
+
         int n = grid.length;
         int m = grid[0].length;
-        int[][] dp = new int[n][m];
 
-        for (int[] ar : dp)
-            Arrays.fill(ar, Integer.MAX_VALUE);
+        int[][] dp = new int[n][m];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (i == 0 && j == 0) {
+                int up = 0;
+                int down = 0; 
+                if(i == 0 && j == 0){
                     dp[i][j] = grid[i][j];
                     continue;
                 }
-                int left = Integer.MAX_VALUE;
-                int top = Integer.MAX_VALUE;
+                if(i == 0){
+                   dp[i][j] = dp[i][j-1];
+                   dp[i][j] += grid[i][j];
+                   continue;
+                }
+                if(j == 0){
+                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] +=grid[i][j];
+                    continue;
+                }
 
-                if (i > 0)
-                    left = dp[i - 1][j];
-                if (j > 0)
-                    top = dp[i][j - 1];
-                dp[i][j] = grid[i][j] + Math.min(left, top);
+            
+                    up = dp[i - 1][j];
+               
+                    down = dp[i][j - 1];
+
+                dp[i][j] = Math.min(up ,  down);
+
+                dp[i][j] += grid[i][j];
+
             }
         }
-        return dp[n - 1][m - 1];
-    }
-
-    int getSum(int[][] grid, int i, int j, int[][] memo) {
-        if (i < 0 || j < 0)
-            return Integer.MAX_VALUE;
-
-        if (i == 0 && j == 0)
-            return grid[i][j];
-
-        if (memo[i][j] != -1)
-            return memo[i][j];
-
-        memo[i][j] = grid[i][j] + Math.min(getSum(grid, i - 1, j, memo), getSum(grid, i, j - 1, memo));
-        return memo[i][j];
-
+      return dp[n-1][m-1];
     }
 }
