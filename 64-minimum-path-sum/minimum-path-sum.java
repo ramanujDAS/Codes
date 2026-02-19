8c96113e5890d6d4b6b86643d4687ffccdbbbf5a
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2026-02-19 22:35:57 +0530

    Time: 5 ms (25.63%) | Memory: 50.4 MB (37.91%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -4,38 +4,31 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
 
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n + 1][m + 1];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int up = 0;
-                int down = 0; 
-                if(i == 0 && j == 0){
-                    dp[i][j] = grid[i][j];
+        
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+
+                if (i == 0 || j == 0) {
+                    dp[i][j] = Integer.MAX_VALUE;
                     continue;
                 }
-                if(i == 0){
-                   dp[i][j] = dp[i][j-1];
-                   dp[i][j] += grid[i][j];
-                   continue;
-                }
-                if(j == 0){
-                    dp[i][j] = dp[i-1][j];
-                    dp[i][j] +=grid[i][j];
+                
+                if(i == 1 && j == 1){
+                    dp[i][j] = grid[i-1][j-1];
                     continue;
                 }
 
-            
-                    up = dp[i - 1][j];
-               
-                    down = dp[i][j - 1];
+                int up = dp[i - 1][j];
+                int down = dp[i][j - 1];
 
-                dp[i][j] = Math.min(up ,  down);
+                dp[i][j] = Math.min(up, down);
 
-                dp[i][j] += grid[i][j];
+                dp[i][j] += grid[i - 1][j - 1];
 
             }
         }
-      return dp[n-1][m-1];
+        return dp[n][m];
     }
 }
