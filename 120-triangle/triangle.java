2487fff3b5c7f1b8b713038a9033e3dab7223dd5
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2026-02-19 23:30:20 +0530

    Time: 3 ms (82.08%) | Memory: 45.5 MB (64.78%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,44 +1,24 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int m =  n;
-        List<List<Integer>> list = new ArrayList<>();
+    public int minimumTotal(List<List<Integer>> t) {
 
-        for(int i = 0 ;i<triangle.size() ;i++){
-            List<Integer> l = new ArrayList<>();
-            for(int j =0 ;j<triangle.get(i).size() ;j++){
-                l.add(-1);
+        int n = t.size();
+        int m = t.get(n - 1).size();
+
+        int[][] dp = new int[n][m];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+               
+               if(i == n-1){
+                  dp[i][j] = t.get(i).get(j);
+                  continue;
+               }
+
+               dp[i][j] = Math.min(dp[i+1][j] , dp[i+1][j+1]) + t.get(i).get(j);
+
             }
-          list.add(l);
         }
 
-       
-       for(int i = triangle.size() -2 ; i>=0 ;i--){
-        for(int j = 0 ;j <triangle.get(i).size() ;j++){
-            int down  =  triangle.get(i+1).get(j);
-            int downSide = triangle.get(i+1).get(j+1);
-
-            int minTotal = triangle.get(i).get(j) + Math.min(down,downSide);
-
-            triangle.get(i).set(j,minTotal);
-        }
-       }
-      return triangle.get(0).get(0);  
-
-        
-    }
-
-    int getTotal(List<List<Integer>> triangle , int i , int j,List<List<Integer>> dp){
-       if(i == triangle.size()) return 0;
-
-       if(dp.get(i).get(j)!= -1) return dp.get(i).get(j);
-        
-        int down = getTotal(triangle , i+1,j,dp);
-        int downRight = getTotal(triangle,i+1,j+1,dp);
-
-        int totalCost = triangle.get(i).get(j) + Math.min(down,downRight);
-        dp.get(i).set(j,totalCost);
-       return  dp.get(i).get(j);
-
+        return dp[0][0];
     }
 }
