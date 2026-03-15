865c5eae6ae36af49bceba9bbaaccd5de404c698
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2026-03-15 23:58:20 +0530

    Time: 2 ms (90.47%) | Memory: 48.1 MB (16.11%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+    int j = -1;
+
+        // Find the first zero
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        if(j == -1) return;
+
+        for(int i = j+1; i < nums.length ;i++){
+
+            if(nums[i] != 0){
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+                j++; 
+            }
+        }
+       
+       
+
+    }
+}
