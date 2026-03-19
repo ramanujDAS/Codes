16258f70325d3b5426baec61495a916b80d06842
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2026-03-19 23:59:35 +0530

    Time: 169 ms (5.43%) | Memory: 47.5 MB (9.20%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,57 +1,34 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
 
+        HashSet<List<Integer>> list = new HashSet<>();
+
         Arrays.sort(nums);
 
-        List<List<Integer>> fourSumList = new ArrayList<>();
         int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int low = j + 1;
+                int high = n - 1;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            for (int j = i + 1; j < nums.length; j++) {
-                if ((j > i + 1)&& (nums[j] == nums[j - 1]))
-                    continue;
-                int k = j + 1;
-                int l = n - 1;
-                while (k < l) {
-
-                    long sum = nums[i];
-                    sum += nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
+                while (low < high) {
+                    long sum = (long)nums[i] + (long)nums[j] + (long)nums[low] + (long)nums[high];
                     if (sum == target) {
-
-                        List<Integer> list = new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        list.add(nums[l]);
-                        fourSumList.add(list);
-                        k++;
-                        l--;
-                        //  while(k < n && nums[ k] == nums[k-1])continue;
-                        //  while(l > 0 && nums[l] == nums[l+1])continue;
-                        while ((k < l) && (nums[k] == nums[k - 1])) {
-                            k++;
-                            continue;
-                        }
-                        while ((k < l) && (nums[l] == nums[l + 1])) {
-                            l--;
-                            continue;
-                        }
-
+                        list.add(Arrays.asList(nums[i], nums[j], nums[low], nums[high]));
+                        low++;
+                        high--;
                     } else if (sum < target) {
-                        k++;
+                        low++;
                     } else {
-                        l--;
+                        high--;
                     }
-
                 }
 
             }
         }
-        return fourSumList;
+        List<List<Integer>> fourSum = new ArrayList<>(list);
+
+        return fourSum;
+
     }
 }
