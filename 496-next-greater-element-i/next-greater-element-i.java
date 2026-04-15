779f84d610db228c28aa8aceb3b057984c7a8f7b
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2026-04-16 00:17:20 +0530

    Time: 8 ms (5.31%) | Memory: 45 MB (76.65%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,25 +1,40 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-       int n = nums2.length;
-     // int[] ans = new int[n] ;
-      Stack<Integer> st = new Stack<>();
-      HashMap<Integer,Integer> ans = new HashMap<>();
-      for(int i = n-1 ;i>=0 ;i--){
-          while(!st.isEmpty() && st.peek()<=nums2[i]){
-            st.pop();
-          }
-         if(st.isEmpty()){
-            ans.put(nums2[i],-1);
-         }else{
-            ans.put(nums2[i],st.peek());
-         }
-        st.push(nums2[i]);
 
-      }
-     int [] anslist = new int[nums1.length];
-      for(int i = 0; i< nums1.length ;i++ ){
-        anslist[i] = ans.getOrDefault(nums1[i],-1);
-      }
-      return anslist;
+       
+       Stack<Integer> st = new Stack<>();
+       int [] ans = new int[nums1.length];
+       int [] nextGrt = new int[nums2.length]; 
+       for(int i = nums2.length-1; i>=0 ; i--){
+              
+              if(st.isEmpty()){
+               nextGrt[i] = -1;
+             st.push(nums2[i]); 
+               continue;
+              }
+
+              while(!st.isEmpty() && nums2[i] > st.peek()){
+                st.pop();
+              }
+            if(st.isEmpty()){
+               nextGrt[i] = -1;
+              }
+            else{
+              nextGrt[i] = st.peek();
+            }
+            st.push(nums2[i]); 
+          
+       } 
+
+       for(int i = 0 ; i<nums1.length;i++)
+       {
+          for(int j = 0; j < nums2.length;j++){
+            if(nums1[i] == nums2[j]){
+                ans[i] = nextGrt[j];
+            }
+          }
+       }
+     return ans;
+
     }
 }
