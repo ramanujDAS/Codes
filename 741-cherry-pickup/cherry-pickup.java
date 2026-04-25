60768d6e6b66df4c01a50eb7868a6f392650b29b
ramanujDAS <87225419+ramanujDAS@users.noreply.github.com>
2026-04-25 19:34:49 +0530

    Time: 14 ms (91.46%) | Memory: 47.1 MB (68.69%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.java
+++ 741-cherry-pickup/cherry-pickup.java
@@ -6,12 +6,12 @@ class Solution {
     public int cherryPickup(int[][] grid) {
         int n = grid.length;
         memo = new Integer[n][n][n];
-        return Math.max(0, cherryPickup(grid, 0, 0, 0, 0));
+        return Math.max(0, cherryPickup(grid, 0, 0, 0));
 
     }
 
-    private int cherryPickup(int[][] grid, int r1, int c1, int r2, int c2) {
-
+    private int cherryPickup(int[][] grid, int r1, int c1, int r2) {
+        int c2 = r1 + c1 -r2;
         if (r1 >= grid.length || r2 >= grid.length || c1 >= grid[0].length || c2 >= grid[0].length) {
             return Integer.MIN_VALUE;
         }
@@ -35,10 +35,10 @@ class Solution {
             cherry += grid[r1][c1] + grid[r2][c2];
         }
 
-        int bothRight = cherryPickup(grid, r1 + 1, c1, r2 + 1, c2); // both in right direction
-        int bothDown = cherryPickup(grid, r1, c1 + 1, r2, c2 + 1); // both in down direction
-        int rightDown = cherryPickup(grid, r1, c1 + 1, r2 + 1, c2); // one right direction and second down direction
-        int downRight = cherryPickup(grid, r1 + 1, c1, r2, c2 + 1); // one down and second right direction
+        int bothRight = cherryPickup(grid, r1 + 1, c1, r2 + 1); // both in right direction
+        int bothDown = cherryPickup(grid, r1, c1 + 1, r2); // both in down direction
+        int rightDown = cherryPickup(grid, r1, c1 + 1, r2 + 1); // one right direction and second down direction
+        int downRight = cherryPickup(grid, r1 + 1, c1, r2); // one down and second right direction
 
         cherry += Math.max(Math.max(bothRight, bothDown), Math.max(rightDown, downRight));
         memo[r1][c1][r2] = cherry;
